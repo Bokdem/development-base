ef3cfc8d2c523d1ec4c6a2ef265f6a00254ecbf0
Bokdem <mathijs@eight.nl>
2015-10-06 11:45:45 +0200

    add ssrver.js // .gitconfig

---
+++ gulp/tasks/server.js
@@ -0,0 +1,19 @@
+'use strict';
+/* globals require */
+
+var gulp = require('gulp');
+// var browserSync = require('browser-sync'); //.reload
+var browserSync = require('browser-sync').create();
+var paths = require('../paths');
+
+var defaultBrowser = defaultBrowser === undefined ? 'default' : defaultBrowser;
+
+gulp.task('server', function() {
+  browserSync.init({
+    browser: defaultBrowser,
+    server: {
+      baseDir: paths.serve,
+      directory: true
+    }
+  });
+});
